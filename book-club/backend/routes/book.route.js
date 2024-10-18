c70c5d7c3baa6a4e7c804f6d40deaf664bd00471
PrachiKatkar16 <prachikatkar116@gmail.com>
2024-10-18 18:53:38 +0530

    changes made

--- book-club/backend/routes/book.route.js
+++ book-club/backend/routes/book.route.js
@@ -1,45 +1,33 @@
 // routes/bookRoutes.js
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 const Book = require('../models/book.model');
 const router = express.Router();
 
-// Multer setup for file upload
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
-
 // Add a new book
 router.post("/", async (req, res) => {
-    const { name, description, price, imageUrl } = req.body;
+  const { name, description, price, imageUrl } = req.body;
   
-    try {
-      const newBook = new Book({
-        name,
-        description,
-        price,
-        imageUrl, // Directly saving the image URL
-      });
-      await newBook.save();
-      res.status(201).json({ message: "Book added successfully", book: newBook });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
-  
+  try {
+    const newBook = new Book({
+      name,
+      description,
+      price,
+      imageUrl, // Saving the image URL directly
+    });
+    await newBook.save();
+    res.status(201).json({ message: "Book added successfully", book: newBook });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 // Delete a book
 router.delete('/:id', async (req, res) => {
   try {
-    await Book.findByIdAndDelete(req.params.id);
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
     res.status(200).json({ message: 'Book deleted successfully' });
   } catch (error) {
     res.status(500).json({ error: 'Failed to delete book' });
